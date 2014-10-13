b063d6e0991cc6dc11dcff7c9b9d3bc6079168ef
Ron Daryl L. Magno <ron.zohan@yahoo.com>
2014-10-13 20:14:15 +0800

    Added a return statement balance

--- javascript/paybill.js
+++ javascript/paybill.js
@@ -1,3 +1,5 @@
+var siteloc = "http://localhost/Paybills";
+var scriptloc = "/scripts/";
 function paybill(accountno,pldtacct,amount)
 {
   $.ajax({
@@ -8,25 +10,24 @@ function paybill(accountno,pldtacct,amount)
              },
       dataType: 'json',
       success: function (res) {
-      		alert(res[0][0]);
+      		console.log(res);
       }
     });
 }
-
 function fetchaccountinfo(accountno)
 {
+	var balance;
 	$.ajax({
       url: siteloc + scriptloc + "paybill.py",
-      data: {accountno:accountno,
-      	pldtacct:pldtacct,
-      	amount:amount
+      data: {accountno:accountno
              },
+      async:false,
       dataType: 'json',
       success: function (res) {
-      		alert(res[0][0]);
+      		balance = res[0][1];
       }
     });
+
+    return balance;
 }
-
-
-} 
+ 
