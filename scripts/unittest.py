6eaef0f1b8a3b2707d2153f73aa227797be91929
Kay Apao <kayapao@yahoo.com>
2014-10-13 23:48:52 +0800

    added unittest.py (new file)

--- scripts/unittest.py
+++ scripts/unittest.py
@@ -1,9 +1,26 @@
+from sendemail import index
+from printreceipt import index
+from getaccountinfo import index
+from paybill import index
+from setreceipt import index
+from sendemail import index
+from login import index
 import unittest
 
 class ArithTest(unittest.TestCase):
-    def run test (self):
-        self.failIf(index(), "Error")
-        self.failUnless(index(), "Another Error")
-        self.failIfEqual(index(), "None")
-        self.failUnlessEqual(index(), "Error again")		
-        
+    def runTest(self):
+        self.failIf(index(1), "Error")
+        self.failUnless(index('December 5, 2014'), "Another Error")
+        self.failIfEqual(index(1), "None")
+        self.failUnlessEqual(index('pea','12345'), "Error again")  
+        self.failIf(index(2,3,60), "Invalid")
+        self.failIf(index(1), "Uncorrect")  
+
+def suite():
+    suite_=unittest.TestSuite()
+    suite_.addTest(ArithTest())
+    return suite_
+
+runner=unittest.TextTestRunner()
+test_suite=suite()
+runner.run(test_suite)
