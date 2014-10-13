11ee2c5934d2449e10789a69879ec05b566a76d6
Kay Apao <kayapao@yahoo.com>
2014-10-13 21:08:07 +0800

    made revisions on receipt.sql

---
+++ scripts/unittest.py
@@ -0,0 +1,8 @@
+import unittest
+
+class ArithTest(unittest.TestCase):
+    def run test (self):
+        self.failIf(index(), "Error")
+        self.failUnless(index(), "Another Error")
+        self.failIfEqual(index(), "None")
+        self.failUnlessEqual(index(), "Error again")		
