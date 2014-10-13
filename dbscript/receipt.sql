d283f51d18fd2cd2625adc7d7c37b2ab655d9a3c
Kay Apao <kayapao@yahoo.com>
2014-10-13 18:47:09 +0800

    addition of new file - receipt.sql

---
+++ dbscript/receipt.sql
@@ -0,0 +1,44 @@
+create table receipt(
+    receipt_id serial primary key,
+    receiptNo text,
+	rDate date,	
+);
+
+
+--HOW TO USE:
+-- SELECT getReceipt(1);
+
+create or replace function getReceipt(in int, out text, out date) 
+	returns setof record as
+
+$$ 
+    select receiptNo, rDate from receipt
+				where receipt_id = $1;
+$$
+ 
+	language 'sql';
+
+
+--HOW TO USE:
+-- SELECT setReceipt('9304230','December 5, 2014');
+
+create or replace function setReceipt(p_receiptNo text, p_rDate date) 
+returns text as
+
+$$
+declare
+  v_receipt_id int; 
+begin
+  select into v_receipt_id receipt_id from receipt
+	where receiptNo = p_receiptNo;
+  
+   insert into events(receiptNo, rDate) 
+					values
+					(p_receiptNo, p_rDate)
+  
+  return 'Ok';
+  end;
+$$
+    language 'plpgsql';
+    
+	
