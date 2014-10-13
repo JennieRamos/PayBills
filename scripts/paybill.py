841b61cc362a27d7fda1ac08ddc2dfef7759d9b2
Ron Daryl L. Magno <ron.zohan@yahoo.com>
2014-10-14 00:00:05 +0800

    Added a line to set a receipt

--- scripts/paybill.py
+++ scripts/paybill.py
@@ -12,7 +12,7 @@ def index(req,accountno,pldtacct,amount):
     x = doSql()
 
     info = x.execqry("select * from accountinfo("+accountno+");",False)
-    if (info != "None"):
+    if (info[0][0] == "None"):
         rets = {'resp':'Something went wrong','receiptNo':'NONE'}
         return json.dumps(rets)
     else:
@@ -23,8 +23,9 @@ def index(req,accountno,pldtacct,amount):
     if int(balance) < int(amount):
         rets = {'resp':'Insufficient Funds','receiptNo':'NONE'}
     else:
-        rets = x.execqry("select * from paybill("+accountno+","+pldtacct+","+amount+");",True)
-        rets = {'resp':'OK','receiptNo':'1234'}
+        rets = x.execqry("select paybill("+accountno+","+pldtacct+","+amount+");",True)
+        receipt = x.execqry("select * from setReceipt(current_date,"+accountno+","+amount+");", True)
+        rets = {'resp':'OK','receiptNo':receipt[0][0]}
 
     return json.dumps(rets)
 
