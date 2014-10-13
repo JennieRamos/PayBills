4d7e51cca917222387423d2052e71e2d50259856
Kay Apao <kayapao@yahoo.com>
2014-10-13 22:38:22 +0800

    made revisions to enable display (printreceipt.py)

--- scripts/printreceipt.py
+++ scripts/printreceipt.py
@@ -7,13 +7,20 @@ except ImportError:
 
 def index(req, receiptNo):	
     receiptNo = cgi.escape(receiptNo)
+    x = doSql()
     rets = x.execqry("select * from getReceipt('" + receiptNo + "');", False)
     results = []
+    items = x.execqry("select accountno from account INNER JOIN receipt on receipt.accountno_FK = \
+    account.accountno where receiptNo = '" + receiptNo + "';", False)
     for ret in rets:
         stringed = map(str, ret)
         results.append(stringed)
-    if stringed != "None":
-        result = {'resp':'OK','receiptNo': results[-1], 'Date' : results[0], 'Account No.': results[1]}
+    for item in items:
+        stringed = map(str, item)
+        results.append(stringed)
+
+    if stringed[-1] != "None":
+        result = {'resp':'OK','receiptNo': results[0][0], 'Date' : results[0][1], 'Account No.': results[1]}
     else:
         result = {'resp':'KO','receiptNo':'NONE'}
     return json.dumps(result)
